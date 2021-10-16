423b2ef69ddf1c496f2b8739b2c06e5390c5c944
Zichen1028 <simonwu0411@gmail.com>
2021-10-17 00:27:57 +1100

    Update Writer.java

--- src/main/java/com/socialmedia/calculator/IOManagement/Writer.java
+++ src/main/java/com/socialmedia/calculator/IOManagement/Writer.java
@@ -24,27 +24,50 @@ public class Writer {
     private Bundle bundle;
 
     /**
+     * Calculate the output for one item
+     * Record the output in a string
+     * @param item  an item in the order list
+     * @return outputOfOneItem  A string contains the output for one item
+     */
+    private String calculateOneItemOutput(Item item) {
+        String outputOfOneItem = "";
+        outputOfOneItem += item.getQuantity() + " " + item.getFormat() + "\r\n";
+        Map<Integer, Long> frequencyMap = item.getResultMap();
+        Map<Integer, BigDecimal> bundles = bundle.getAllProductFormatAndBundleMap().get(item.getFormat());
+        BigDecimal subTotal = new BigDecimal(0);
+        for (Map.Entry<Integer, Long> entry : frequencyMap.entrySet()) {
+            BigDecimal priceOfOneBundle = new BigDecimal(entry.getValue()).multiply(bundles.get(entry.getKey()));
+            subTotal = subTotal.add(priceOfOneBundle);
+            outputOfOneItem += "  " + entry.getValue() + "*" + entry.getKey() + " $" + priceOfOneBundle + "\r\n";
+        }
+        outputOfOneItem += "subTotal: " + "$" + subTotal + "\r\n";
+        return outputOfOneItem;
+    }
+
+    /**
+     * calculate the output for all items
+     * record the output content in a string for later writing
+     * @return      a string to be written in the output file
+     */
+    private String calculateOutputContent() {
+        List<Item> itemList = order.getOrderList();
+        String outputContent = "";
+        for (Item item : itemList) {
+            String outputOfOneItem = calculateOneItemOutput(item);
+            outputContent += outputOfOneItem;
+        }
+        return outputContent;
+    }
+
+    /**
      * Process the result of each item
      * Then process and write the result to the output file
      * @param outputFileName    The name of the output file
      */
     public void writeResult(String outputFileName) {
         try (BufferedWriter writer = new BufferedWriter(new FileWriter(outputFileName))) {
-            List<Item> itemList = order.getOrderList();
-            for (Item item: itemList) {
-                int quantity = item.getQuantity();
-                String format = item.getFormat();
-                writer.write(quantity + " " + format + "\n");
-                Map<Integer, Long> frequencyMap = item.getResultMap();
-                Map<Integer, BigDecimal> bundles = bundle.getAllProductFormatAndBundleMap().get(format);
-                BigDecimal subTotal = new BigDecimal(0);
-                for (Map.Entry<Integer, Long> entry: frequencyMap.entrySet()) {
-                    BigDecimal priceOfOneBundle = new BigDecimal(entry.getValue()).multiply(bundles.get(entry.getKey()));
-                    subTotal = subTotal.add(priceOfOneBundle);
-                    writer.write("  " + entry.getValue() + "*" + entry.getKey() + " $" + priceOfOneBundle + "\n");
-                }
-                writer.write("subTotal: " + "$" + subTotal + "\n");
-            }
+            String outputContent = calculateOutputContent();
+            writer.write(outputContent);
         } catch (IOException ex) {
             Logger.getLogger("Writing file exception!");
         }
