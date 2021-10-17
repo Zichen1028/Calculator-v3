b3eee7ea02bee23f0bee756c5666e9f9f10c2707
Zichen1028 <simonwu0411@gmail.com>
2021-10-17 21:09:25 +1100

    Update CalculatedResultProcessorTest.java

--- src/test/java/com/socialmedia/calculator/calculatedresultprocessor/CalculatedResultProcessorTest.java
+++ src/test/java/com/socialmedia/calculator/calculatedresultprocessor/CalculatedResultProcessorTest.java
@@ -1,7 +1,20 @@
 package com.socialmedia.calculator.calculatedresultprocessor;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class CalculatedResultProcessorTest {
 
+    @Test
+    void transferAndSetCalculatedResult() {
+    }
+
+    @Test
+    void getBundle() {
+    }
+
+    @Test
+    void setBundle() {
+    }
 }
